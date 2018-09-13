fd63fda81134c2b1fbff2e85945390a63409ee7d
Antoine Nourris <nourrisantoine44@gmail.com>
2018-09-13 21:42:11 +0200

    version finale

--- map.js
+++ map.js
@@ -14,98 +14,101 @@ function regular_map() {
         center: var_location,
         zoom: 13
 
-    };
- 
-    function setMarkers(map) {
-        // Adds markers to the map.
-      
-        // Marker sizes are expressed as a Size of X,Y where the origin of the image
-        // (0,0) is located in the top left of the image.
-      
-        // Origins, anchor positions and coordinates of the marker increase in the X
-        // direction to the right and in the Y direction down.
-        var image = {
-          url: 'https://image.ibb.co/eQZjS9/arc_de_triomphe_1283422_1920.jpg',
-          // This marker is 20 pixels wide by 32 pixels high.
-          size: new google.maps.Size(30, 40)
-          // The origin for this image is (0, 0).
-         // origin: new google.maps.Point(0, 0),
-          // The anchor for this image is the base of the flagpole at (0, 32).
-         // anchor: new google.maps.Point(0, 32)
-        };
-        // Shapes define the clickable region of the icon. The type defines an HTML
-        // <area> element 'poly' which traces out a polygon as a series of X,Y points.
-        // The final coordinate closes the poly by connecting to the first coordinate.
-         //var shape = {
-        //coords: [1, 1, 1, 20, 18, 20, 18, 1],
-        //type: 'poly'
-        //};
-        /*
-        for (var i = 0; i < beaches.length; i++) {
-          var beach = beaches[i];
-          var marker = new google.maps.Marker({
-            position: {lat: beach[1], lng: beach[2]},
-            map: map,
-            icon: image,
-            shape: shape,
-            title: beach[0],
-            zIndex: beach[3]
-          });*/
-
-          var marker = new google.maps.Marker({
-            position: arch,
-            map: var_map,
-            title: 'Triumph Arch',
-            icon : image,
-            zIndex : 2
-            
-        });
-    
-        }
-
-    
-    var var_map = new google.maps.Map(document.getElementById("map-container"),
-        var_mapoptions);
-
-    setMarkers(var_map);    
-
-    var_map.setOptions({draggable: false, zoomControl: false, scrollwheel: false, disableDoubleClickZoom: true}); 
-
+	};
+	
+	var map = new google.maps.Map(document.getElementById("map-container"),
+		var_mapoptions);
+		
+	map.setOptions({draggable: false, zoomControl: false, scrollwheel: false, disableDoubleClickZoom: true});
+	
+	var marker_arch = new google.maps.Marker({
+		position: arch,
+		map: map,
+		title: 'Triumph Arch',
+		
+		
+	});
 
    
-    var marker = new google.maps.Marker({
+    var marker_louvre = new google.maps.Marker({
         position: louvre,
-        map: var_map,
+        map: map,
         title: 'Louvre'
     }); 
 
-    var marker = new google.maps.Marker({
+    var marker_sacre = new google.maps.Marker({
         position: sacre,
-        map: var_map,
+        map: map,
         title: 'sacre'
     });  
 
-    var marker = new google.maps.Marker({
+    var marker_dame = new google.maps.Marker({
         position: dame,
-        map: var_map,
+        map: map,
         title: 'dame'
     });  
 
-    var marker = new google.maps.Marker({
+    var marker_moulin = new google.maps.Marker({
         position: moulin,
-        map: var_map,
+        map: map,
         title: 'moulin'
     });  
 
-    var marker = new google.maps.Marker({
+    var marker_eiffel = new google.maps.Marker({
         position: eiffel,
-        map: var_map,
+        map: map,
         title: 'eiffel'
-    });  
+	});  
+	
+	marker_arch.addListener('click', function() {
+		document.getElementById("switch").innerHTML="<img class = \"col-md-12 mt-5\" src='https://image.ibb.co/eQZjS9/arc_de_triomphe_1283422_1920.jpg'>";
 
-    marker.setMap(var_map);
+	  });
+
+	marker_louvre.addListener('click', function() {
+		document.getElementById("switch").innerHTML="<img class = \"col-md-12 mt-5\" src='https://image.ibb.co/ig0cEp/louvre_102840_1280.jpg'>";
+
+	  });
+
+	marker_sacre.addListener('click', function() {
+		document.getElementById("switch").innerHTML="<img class = \"col-md-12 mt-5\" src='https://image.ibb.co/g4zW4p/sacre_couer_356123_1280.jpg'>";
+
+	  });
+
+	marker_dame.addListener('click', function() {
+		document.getElementById("switch").innerHTML="<img class = \"col-md-12 mt-5\" src='https://image.ibb.co/fhU179/cathedral_3599931_1920.jpg'>";
+
+	  });
+
+	marker_moulin.addListener('click', function() {
+		document.getElementById("switch").innerHTML="<img class = \"col-md-12 mt-5\" src='https://image.ibb.co/dUw3LU/moulin_rouge_1050325_1920.jpg'>";
+
+	  });
+
+	marker_eiffel .addListener('click', function() {
+		document.getElementById("switch").innerHTML="<img class = \"col-md-12 mt-5\" src='https://image.ibb.co/bxsaEp/eiffel_tower_1280022_1280.jpg'>";
+
+	  });
+  
+  
+
+  
+  
+
+
+ 
+         
+        }
     
-}
+
+     
+
+
+	
+
+    
+    
+
 
 // Initialize maps
 google.maps.event.addDomListener(window, 'load', regular_map);
